9a392adeda710ec19052216c981f742a2e6225cc
ohmin839 <ohmin839.echoda.resident@gmail.com>
2022-08-14 20:17:12 +0900

    step23: core module

---
+++ pybuben-deeplearning/pybuben/deeplearning/__init__.py
@@ -0,0 +1,20 @@
+is_simple_core = True
+
+if is_simple_core:
+    from .core_simple import Variable
+    from .core_simple import Function
+    from .core_simple import using_config
+    from .core_simple import no_grad
+    from .core_simple import as_array
+    from .core_simple import as_variable
+    from .core_simple import setup_variable
+else:
+    from .core import Variable
+    from .core import Function
+    from .core import using_config
+    from .core import no_grad
+    from .core import as_array
+    from .core import as_variable
+    from .core import setup_variable
+    
+setup_variable()
